72a48b8ac4cc7d08e2dc27eae3e1eff4d9b44c5e
= <=>
2021-02-14 19:35:14 +0100

    Added tailwind css and styled components

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,53 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const Container = styled.div`
+  ${tw`bg-blue-100`}
+  height: 100%;
+  width: 100%;
+  overflow: auto;
+`;
+
+const Content = styled.main`
+  ${tw`m-2 p-8`}
+`;
+
+const Header = styled.h1`
+  ${tw`font-bold text-6xl`}
+`;
+
+const StyledLink = styled.a`
+  ${tw`text-xl`}
+`;
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Container>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Next JS with Tailwind and Styled-Components</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+      <Content>
+        <Header>
+          This is a basic Next JS site with Styled-Component and Tailwind CSS
+        </Header>
+        <ul>
+          <li>
+            <StyledLink href="https://styled-components.com/">
+              Styled-Components
+            </StyledLink>
+          </li>
+          <li>
+            <StyledLink href="https://tailwindcss.com/">TailwindCSS</StyledLink>
+          </li>
+          <li>
+            <StyledLink href="https://github.com/ben-rogerson/twin.macro">
+              Twin.Macro
+            </StyledLink>
+          </li>
+        </ul>
+      </Content>
+    </Container>
+  );
 }
